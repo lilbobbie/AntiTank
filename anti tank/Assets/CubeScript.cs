5cc73be4a291e93083d4137b40d1f983e06848d7
Robbe R <robberillaerts.rr@gmail.com>
2022-08-10 23:13:58 +0200

    Implemented test level and basic controls

---
+++ anti tank/Assets/CubeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    bool isPressed = false;
+
+    public float rotSpeed;
+
+    public void pressed()
+    {
+        isPressed = true;
+    }
+    public void released()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
+        }
+    }
+}
