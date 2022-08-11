9b98c418267fe3e58fc46d4d021c340526fa53de
Lucas Van Schaeren <lucas.vanschaeren@student.kdg.be>
2022-08-11 14:02:53 +0200

    Arduino controler script test

---
+++ anti tank/Assets/Scripts/Move.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Move : MonoBehaviour
+{
+
+	SerialPort sp = new SerialPort("COM3", 9600); // set port of your arduino connected to computer
+
+	void Start()
+	{
+		sp.Open();
+		sp.ReadTimeout = 1;
+	}
+
+	void Update()
+	{
+		if (sp.IsOpen)
+		{
+			try
+			{
+				if (sp.ReadByte() == 1)
+				{
+					transform.Translate(Vector3.left * Time.deltaTime * 5);
+				}
+				if (sp.ReadByte() == 2)
+				{
+					transform.Translate(Vector3.right * Time.deltaTime * 5);
+				}
+				if (sp.ReadByte() == 3)
+				{
+					transform.Translate(Vector3.back * Time.deltaTime * 5);
+				}
+				if (sp.ReadByte() == 4)
+				{
+					transform.Translate(Vector3.forward * Time.deltaTime * 5);
+				}
+			}
+			catch (System.Exception)
+			{
+			}
+		}
+	}
+}
