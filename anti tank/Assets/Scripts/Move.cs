9513f1c657c83de990b096060f615aecc7da6d34
Lucas Van Schaeren <lucas.vanschaeren@student.kdg.be>
2022-08-11 16:05:35 +0200

    Added a hit sound script. Testing to see how it works

--- anti tank/Assets/Scripts/Move.cs
+++ anti tank/Assets/Scripts/Move.cs
@@ -7,7 +7,7 @@ public class Move : MonoBehaviour
 {
 
 	SerialPort sp = new SerialPort("COM3", 9600); // set port of your arduino connected to computer
-
+	public int walkSpeed = 10;
 	void Start()
 	{
 		sp.Open();
@@ -16,25 +16,26 @@ public class Move : MonoBehaviour
 
 	void Update()
 	{
+
 		if (sp.IsOpen)
 		{
 			try
 			{
 				if (sp.ReadByte() == 1)
 				{
-					transform.Translate(Vector3.left * Time.deltaTime * 5);
+					transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
 				}
 				if (sp.ReadByte() == 2)
 				{
-					transform.Translate(Vector3.right * Time.deltaTime * 5);
+					transform.Translate(Vector3.left * Time.deltaTime * walkSpeed);
 				}
 				if (sp.ReadByte() == 3)
 				{
-					transform.Translate(Vector3.back * Time.deltaTime * 5);
+					transform.Translate(Vector3.back * Time.deltaTime * walkSpeed);
 				}
 				if (sp.ReadByte() == 4)
 				{
-					transform.Translate(Vector3.forward * Time.deltaTime * 5);
+					transform.Translate(Vector3.right* Time.deltaTime * walkSpeed);
 				}
 			}
 			catch (System.Exception)
---
+++ anti tank/Assets/Scripts/hitSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class hitSound : MonoBehaviour
+{
+
+	SerialPort sp = new SerialPort("COM3", 9600); // set port of your arduino connected to computer
+	public bool hit = false;
+	private char[] txChars = { 'A', 'U' };
+	void Start()
+	{
+		sp.Open();
+		sp.ReadTimeout = 1;
+	}
+
+	void Update()
+	{
+		if (sp.IsOpen)
+		{
+			try
+			{
+				if (hit == true)
+				{
+					sp.Write(txChars, 0, 1);
+					
+				}
+			}
+			catch (System.Exception)
+			{
+			}
+		}
+	}
+}
