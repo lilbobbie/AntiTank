22630b71dc1fa5ea0eb5830127ffa035234ba714
Lucas Van Schaeren <lucas.vanschaeren@student.kdg.be>
2022-08-14 20:27:21 +0200

    improved waypoint folllowing script

--- anti tank/Assets/WaypointScript.cs
+++ anti tank/Assets/WaypointScript.cs
@@ -5,56 +5,41 @@ using UnityEngine.AI;
 
 public class WaypointScript : MonoBehaviour
 {
-    public List<Transform> waypoints = new List<Transform>();
-    private Transform targetWaypoint;
-    private int targetWaypointIndex = 0;
-    private float minDistance = 0.1f;
-    private float lastWaypointIndex;
+    public Transform[] waypoints;
+    public int speed;
 
-    public float movementSpeed = 3.0f;
-    public float rotationSpeed = 2.0f;
+    private int waypointIndex;
+    private float distance;
 
     void Start()
     {
-        lastWaypointIndex = waypoints.Count - 1;
-        targetWaypoint = waypoints[targetWaypointIndex];
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        float movementStep = movementSpeed * Time.deltaTime;
-        float rotationStep = rotationSpeed * Time.deltaTime;
-
-        Vector3 directionToTarget = targetWaypoint.position - transform.position;
-        Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
-
-        transform.rotation = rotationToTarget;
-
-        Debug.DrawRay(transform.position, transform.forward * 50f,Color.green,0f);
-        Debug.DrawRay(transform.position, directionToTarget, Color.red, 0f);
-
-        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
-        CheckDistanceToWaypoint(distance);
-
-        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
-    }
-    
-    void CheckDistanceToWaypoint(float currentDistance)
-    {
-        if (currentDistance <= minDistance)
+        distance = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if (distance < 5f)
         {
-            targetWaypointIndex++;
-            UpdateTargetWaypoint();
+            IncreaseIndex();
         }
+        Patrol();
     }
 
-    void UpdateTargetWaypoint()
+    void Patrol()
     {
-        if (targetWaypointIndex > lastWaypointIndex)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.Length)
         {
-            targetWaypointIndex = 0;
+            waypointIndex = 0;
         }
-        targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(waypoints[waypointIndex].position);
     }
 }
