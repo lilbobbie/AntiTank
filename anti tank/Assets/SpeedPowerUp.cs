9f35c8e4fb238df2b1d9a4f7cde14b51ab6b9db8
Robbe R <robberillaerts.rr@gmail.com>
2022-08-14 22:46:12 +0200

    Added SpeedPowerUp
    Need to implement fleeing behaviour

---
+++ anti tank/Assets/SpeedPowerUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public GameObject weapon;
+    public int powerUpDuration;
+
+    float preTBS;
+    float preRT;
+    private void Awake()
+    {
+        preTBS = weapon.GetComponent<RocketLauncher>().timeBetweenShooting;
+        preRT = weapon.GetComponent<RocketLauncher>().reloadTime;
+    }
+
+
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PowerUpManager.Instance.StartCoroutine(PowerUpManager.Instance.PowerUp(powerUpDuration));
+
+            Destroy(this.gameObject);
+        }
+    }
+}
