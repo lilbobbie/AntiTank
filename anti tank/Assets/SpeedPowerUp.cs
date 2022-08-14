577cf9ab2f2f4362f75fb06a17d7173852226c9e
Robbe R <robberillaerts.rr@gmail.com>
2022-08-14 22:57:18 +0200

    Made powerup flee

--- anti tank/Assets/SpeedPowerUp.cs
+++ anti tank/Assets/SpeedPowerUp.cs
@@ -6,6 +6,10 @@ public class SpeedPowerUp : MonoBehaviour
 {
     public GameObject weapon;
     public int powerUpDuration;
+    public float speed;
+    public Vector3 direction;
+    public Transform seekTarget;
+    public float fleeDistance = 5;
 
     float preTBS;
     float preRT;
@@ -15,6 +19,21 @@ public class SpeedPowerUp : MonoBehaviour
         preRT = weapon.GetComponent<RocketLauncher>().reloadTime;
     }
 
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, seekTarget.position) < fleeDistance)
+        {
+            Vector3 newDirection = transform.position - seekTarget.position;
+            newDirection = new Vector3(newDirection.x, 0, newDirection.z);
+            newDirection = newDirection.normalized;
+            direction = newDirection;
+        }
+        else
+        {
+            direction = Vector3.zero;
+        }
+        transform.position += speed * direction * Time.deltaTime;
+    }
 
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
