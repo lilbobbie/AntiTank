9bec0d9503d1b4436370fed76e38453b66ed60ba
Lucas Van Schaeren <lucas.vanschaeren@student.kdg.be>
2022-08-15 21:59:14 +0200

    continued trying to fix arduino input lag

--- anti tank/Assets/ArduinoMovement.cs
+++ anti tank/Assets/ArduinoMovement.cs
@@ -13,8 +13,7 @@ public class ArduinoMovement : MonoBehaviour
 	void Start()
 	{
 		sp.Open();
-		sp.ReadTimeout = 25;
-
+		sp.ReadTimeout = 25 ;
 	}
 
 	void Update()
@@ -24,7 +23,29 @@ public class ArduinoMovement : MonoBehaviour
 		{
 			try
 			{
-				Movement();
+               
+                if (hit == true)
+				{
+					HitSound();
+				}
+				if (sp.ReadByte() == 1)
+				{
+					transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
+				}
+				if (sp.ReadByte() == 2)
+				{
+					transform.Translate(Vector3.left * Time.deltaTime * walkSpeed);
+				}
+				if (sp.ReadByte() == 3)
+				{
+					transform.Translate(Vector3.back * Time.deltaTime * walkSpeed);
+				}
+				if (sp.ReadByte() == 4)
+				{
+					transform.Translate(Vector3.right* Time.deltaTime * walkSpeed);
+				}
+				
+				
 			}
 			catch (System.Exception)
 			{
@@ -39,7 +60,6 @@ public class ArduinoMovement : MonoBehaviour
 
 	public void AmmoLed(int ammo)
     {
-
 		switch (ammo)
 		{
 			case 0:
@@ -60,30 +80,5 @@ public class ArduinoMovement : MonoBehaviour
 			default:
 				break;
 		}
-	}
-
-	public void Movement()
-    {
-		if(sp.ReadByte() == 1)
-				{
-			transform.Translate(Vector3.forward + Camera.main.transform.forward * Time.deltaTime * walkSpeed);
-			Debug.Log("Forward");
-		}
-		if (sp.ReadByte() == 2)
-		{
-			transform.Translate(Vector3.left - Camera.main.transform.right * Time.deltaTime * walkSpeed);
-			Debug.Log("Left");
-		}
-		if (sp.ReadByte() == 3)
-		{
-			transform.Translate(Vector3.back - Camera.main.transform.forward* Time.deltaTime * walkSpeed);
-			Debug.Log("Back");
-		}
-		if (sp.ReadByte() == 4)
-		{
-			transform.Translate(Vector3.right + Camera.main.transform.right  * Time.deltaTime * walkSpeed);
-			Debug.Log("Right");
-		}
-
-	}
+    }
 }
